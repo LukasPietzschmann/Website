fb73903b7cc849a927c834611627eec11cd60da7
Lukas Pietzschmann <lukaspie00@gmail.com>
2021-04-12 15:39:46 +0200

    Added Columns to Project-Table

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -19,20 +19,24 @@ function AllProjects({ className }) {
 			<table className='table table-hover'>
 				<thead>
 					<tr>
-						<th scope='col'>#</th>
+						<th scope='col' className='d-none d-md-table-cell'>#</th>
 						<th scope='col'>Name</th>
 						<th scope='col'>Description</th>
 						<th scope='col'>Language</th>
+						<th scope='col' className='d-none d-md-table-cell'>Last Updated</th>
+						<th scope='col' className='d-none d-lg-table-cell'>License</th>
 					</tr>
 				</thead>
 				<tbody>
-					{repos.map(({ name, description, language, html_url }, index) => {
+					{repos.map(({ name, description, language, html_url, pushed_at, license }, index) => {
 						return (
 							<tr key={index} onClick={() => window.location = html_url} style={{cursor: 'pointer'}}>
-								<th scope='row'>{index + 1}</th>
+								<th scope='row' className='d-none d-md-table-cell'>{index + 1}</th>
 								<td>{name}</td>
 								<td>{description}</td>
 								<td>{language}</td>
+								<td className='d-none d-md-table-cell'>{['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'][new Date(pushed_at).getMonth()]} {new Date(pushed_at).getDate()} {new Date(pushed_at).getFullYear()}</td>
+								<td className='d-none d-lg-table-cell'>{license ? license.name : ''}</td>
 							</tr>
 						);
 					})}
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -5,7 +5,7 @@ function FrontPage({ className, style }) {
 	return (
 		<div className={`row ${className}`} style={style}>
 			<SelfIntroduction className='col-md-4 my-md-3 m-md-3'/>
-			<AllProjects className='col-md my-md-3 mx-md-3'/>
+			<AllProjects className='col-md-7 my-md-3 mx-md-3'/>
 		</div>
 	);
 }
